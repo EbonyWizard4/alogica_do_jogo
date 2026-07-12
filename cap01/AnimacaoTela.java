55269ddf190d1da0e521e28669efca3e1ae1f104
EbonyWizard4 <antonio.jhone@hotmail.com>
2026-07-12 00:11:03 -0300

    feat: Criado primeira animacao

--- cap01/AnimacaoTela.java
+++ cap01/AnimacaoTela.java
@@ -9,42 +9,44 @@ public class AnimacaoTela extends JFrame {
 	private int ct; //xontador
 	private boolean anima = true;
 
-	public void iniciaAbimacao() {
+	public void iniciaAnimacao() {
 		long prxAtualizacao = 0;
 		while (anima) {
-			if (System.currentTimeMillis() >= prxAtualizacao)
+			if (System.currentTimeMillis() >= prxAtualizacao) {
 				ct++;
 				tela.repaint();
 
 				prxAtualizacao = System.currentTimeMillis() + fps;
 
-			if (ct == 100)
+			if (ct == 100) 
 				anima = false;
-
+			}
 		}
-
 	}
 	
-	public Janela() {
+	public AnimacaoTela() {
 		tela = new JPanel() {
 			@Override
 			public void paintComponent(Graphics g) {
 				super.paintComponent(g);
 
+				//Limpando os desenhos anteriores
+				g.setColor(Color.WHITE);
+				g.fillRect(0, 0, tela.getWidth(), tela.getHeight());
 
 			// A pintura ocorre aqui
 				g.setColor(Color.BLUE);
-				g.drawLine(0, 240, 640, 240);
-				g.drawRect(10, 25, 20, 20);
-				g.drawOval(30, 20, 40, 30);
+				g.drawLine(0, 240 + ct, 640, 240 + ct);
+				g.drawRect(10, 25 + ct, 20, 20);
+				g.drawOval(30 + ct, 20, 40, 30);
 
 				g.setColor(Color.YELLOW);
-				g.drawLine(320, 0, 320, 4800);
-				g.fillRect(110, 125, 120, 120);
-				g.fillOval(230, 220, 240, 230);
+				g.drawLine(320 - ct, 0, 320 - ct, 4800);
+				g.fillRect(110, 125, 120 - ct, 120 - ct);
+				g.fillOval(230, 220, 240 + ct, 230);
 
 				g.setColor(Color.RED);
-				g.drawString("Eu seria um otimo Score!", 5, 10);
+				g.drawString("Eu seria um otimo Score!" + ct, 5, 10);
 
 			}
 		};
@@ -56,12 +58,12 @@ public class AnimacaoTela extends JFrame {
 		getContentPane().add(tela);
 		setLocationRelativeTo(null);
 		setVisible(true);
-		//tela.repaint();
+		tela.repaint();
 	}
 
 
 	public static void main(String[] args) {
-		new Janela();
+		AnimacaoTela anima = new AnimacaoTela();
+		anima.iniciaAnimacao();
 	}
-
 }
